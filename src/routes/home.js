669f48ca301808e266c63ce6160c27789ad62817
erickyb <richardyb1985@gmail.com>
2023-05-23 00:34:54 -0500

    una nueva ruta posts

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const axios = require("axios");
 
 const router = Router();
 
@@ -34,5 +35,11 @@ router.get("/about", (req, res) => {
 router.get("/dashboard", (req, res) => {
   res.render('dashboard');
 });
-
+router.get("/posts", async (req, res) => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+  //console.log(response.data)
+  res.render('posts', {
+    posts: response.data
+  });
+});
 module.exports = router;

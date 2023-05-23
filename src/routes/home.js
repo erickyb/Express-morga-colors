d9c25daafa7c5a2b6e4d362bda05278e4b6a7cd3
erickyb <richardyb1985@gmail.com>
2023-05-22 21:53:36 -0500

    se utilizo if y for con express

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,14 +3,32 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
+  let isActive = false;
+  const users = [
+    {
+      id: 1,
+      name: "Dios",
+      lastname: "Maria"
+    },
+    {
+      id: 2,
+      name: "Roxana",
+      lastname: "Emily y Richard"
+    }
+
+  ]
+
   res.render("index", {
-    title: 'Index page'
+    title: 'Index page',
+    isActive,
+    users
   });
 });
 
 router.get("/about", (req, res) => {
   const title = 'Acerca de '
-  res.render('about');
+  res.render('about', {
+  });
 });
 
 router.get("/dashboard", (req, res) => {
